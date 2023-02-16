996e096ac4db9370b6862c1e7a48ef25d6e412c1
o <nesubludai@gmail.com>
2023-02-16 15:57:50 +0300

    New tests of login link on product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,17 +12,24 @@ class ProductPage(BasePage):
         self.should_be_basket_alert()
         self.basket_cost_equals_product_cost()
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_ALERT), 'Success message presented,' \
+                                                                                ' should not be'
+
+    def success_message_should_disappear_after_adding_product_to_basket(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_ALERT), 'Success message should disappear, it is not'
+
     def add_product_to_basket(self):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
-        self.solve_quiz_and_get_code()
+        # self.solve_quiz_and_get_code()
 
     def should_be_product_added_to_basket_alert(self):
         assert self.is_element_present(*ProductPageLocators.SUCCESS_ALERT), 'Product added ' \
-                                                                                            'to basket alert not found'
+                                                                            'to basket alert not found'
 
     def product_name_equals_product_name_in_product_added_to_basket_alert(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == self.browser.\
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == self.browser. \
             find_element(*ProductPageLocators.PRODUCT_NAME_IN_ALERT).text, 'Product name in alert isn\'t correct'
 
     def should_be_basket_alert(self):
@@ -31,4 +38,3 @@ class ProductPage(BasePage):
     def basket_cost_equals_product_cost(self):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text == \
                self.browser.find_element(*ProductPageLocators.PRODUCT_COST_IN_ALERT).text, 'Cost in alert isn\' correct'
-
--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 from pages.product_page import ProductPage
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -21,3 +22,44 @@ def test_quest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_product_to_basket()
     page.guest_should_see_product_added_to_basket()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappear_after_adding_product_to_basket()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
